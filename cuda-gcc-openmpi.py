574e56ee49868fca090ac4bf65f4f25d19452741
27tech <denis@27tech.co>
2019-03-12 23:24:13 +0200

    hpc containers

---
+++ cuda-gcc-openmpi.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import argparse
+import hpccm
+from hpccm.building_blocks import gnu, mlnx_ofed, openmpi, pgi, gdrcopy, ucx, mvapich2_gdr, xpmem, knem
+from hpccm.primitives import baseimage
+
+parser = argparse.ArgumentParser(description='HPCCM Tutorial')
+parser.add_argument('--format', type=str, default='docker',
+                    choices=['docker', 'singularity'],
+                    help='Container specification format (default: docker)')
+
+args = parser.parse_args()
+
+Stage0 = hpccm.Stage()
+
+py_ver = '3.6'
+mlnx_ofed_version = '4.5-1.0.1.0'
+
+compiler = gnu()
+
+# compiler = pgi(eula=True)
+
+Stage0 += baseimage(image='tataucloud/python-cuda:{}'.format(py_ver))
+Stage0 += mlnx_ofed(version=mlnx_ofed_version)
+Stage0 += gdrcopy(version='1.3')
+Stage0 += compiler
+# Stage0 += xpmem()
+# Stage0 += knem()
+Stage0 += ucx(version='1.5.0', cuda=True, gdrcopy=True)
+Stage0 += openmpi(cuda=True, infiniband=True, toolchain=compiler.toolchain, version='3.1.2', ucx='/usr/local/ucx')
+# Stage0 += mvapich2_gdr(version='2.3', cuda_version='9.0')
+
+hpccm.config.set_container_format(args.format)
+
+print(Stage0)
